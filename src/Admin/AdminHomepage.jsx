c6e769cc542ca265944804dfe5c21421300f0216
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-11-25 08:25:19 +0200

    updated

--- src/Admin/AdminHomepage.jsx
+++ src/Admin/AdminHomepage.jsx
@@ -1,179 +1,167 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
-import {app, database, imageDb} from '../firebaseConfig';
+import React, { useState, useEffect } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
+import { app, database, imageDb } from '../firebaseConfig';
 import { collection, addDoc, getDocs } from 'firebase/firestore';
-import {v4 as uuidv4} from "uuid";
- import {
-    CardMeta,
-    CardHeader,
-    CardDescription,
-    CardContent,
-    Card,
-    Icon,
-    Image,
-  } from 'semantic-ui-react'
- 
+import { v4 as uuidv4 } from 'uuid';
+import {
+  CardMeta,
+  CardHeader,
+  CardDescription,
+  CardContent,
+  Card,
+  Icon,
+  Image,
+} from 'semantic-ui-react';
 
-export default function AdminHomePage(){
-    const storage = getStorage();
-    const collectionStore = collection(database, 'rooms');
-    const [room, setRoom] = useState({
-        // img: "",
-        room:"",
-        description: "",
-        price: "",
-        capacity: "",
-        availability: "",
-    });
+export default function AdminHomePage() {
+  const storage = getStorage();
+  const collectionStore = collection(database, 'rooms');
 
-    const [img, setImg] =useState('');
+  const [room, setRoom] = useState({
+    room: '',
+    description: '',
+    price: '',
+    capacity: '',
+    availability: '',
+  });
 
-    useEffect(() => {
-      const fetchRooms = () => {
-          getDocs(collectionStore)
-              .then((snapshot) => {
-                  const roomsArray = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-                  console.log("Rooms Data:", roomsArray);
-                  setRoom(roomsArray);
-              })
-              .catch((error) => {
-                  console.error('Error fetching rooms:', error);
-              });
-      };
-      fetchRooms();
+  const [img, setImg] = useState('');
+
+  useEffect(() => {
+    const fetchRooms = () => {
+      getDocs(collectionStore)
+        .then((snapshot) => {
+          const roomsArray = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+          console.log('Rooms Data:', roomsArray);
+          setRoom(roomsArray);
+        })
+        .catch((error) => {
+          console.error('Error fetching rooms:', error);
+        });
+    };
+    fetchRooms();
   }, []);
 
-    // useEffect(() =>{
-    //   const roomRef = ref(database, 'users');
-    //   get(roomRef).then((snapshot) => {
-    //     if(snapshot.exists()){
-    //       const roomsArray = Object.entries(snapshot.val()).map(([id, data]) =>({
-    //         id, ...data,
-    //       }));
-    //       setRoom(roomsArray);
-    //     }
-    //   }).catch((error) =>{
-    //     console.error(error);
-    //   })
-    // }, []);
-
-    function handleSubmit(event){
-      event.preventDefault();
-      console.log(room);
-      addDoc(collectionStore, {
-        // img: room.img,
-        room: room.room,
-        description:room.description, 
-        price: room.price,
-        capacity: room.capacity,
-        availability: room.availability
-
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(room);
+    addDoc(collectionStore, {
+      room: room.room,
+      description: room.description,
+      price: room.price,
+      capacity: room.capacity,
+      availability: room.availability, // Missing closing bracket was here
+    })
+      .then(() => {
+        alert('Room added');
       })
-      .then(()=>{
-        alert("Room added")
-      })
-      .catch((err)=>{
+      .catch((err) => {
         alert(err.message);
+      });
+  }
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    console.log(value);
+    setRoom((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  function handleClick(e) {
+    let imgRef = ref(imageDb, `files/${uuidv4()}`);
+    uploadBytes(imgRef, img)
+      .then((snapshot) => {
+        getDownloadURL(snapshot.ref)
+          .then((url) => {
+            console.log(url);
+            setRoom((prevState) => ({
+              ...prevState,
+              img: url,
+            }));
+          })
+          .catch((err) => console.error(err));
       })
-    }
+      .catch((err) => console.log(err));
+  }
 
-    
-
-    function handleInputChange(e) {
-      const { name, value } = e.target;
-      console.log(value)
-      setRoom(prevState => ({
-          ...prevState,
-          [name]: value
-      }));
-    }
-
-    function handleClick(e){
-      let imgRef = ref(imageDb, `files/${uuidv4()}`)
-    uploadBytes(imgRef,img).then((snapshot)=>{
-      getDownloadURL(snapshot.ref).then((url)=>{
-        console.log(url)
-        setRoom(prevState => ({
-          ...prevState,
-          img: url
-      }));
-      }).catch((err)=>console.error(err))
-    }).catch((err)=>console.log(err))
-    }
-
-    // const handleUpload = (e) =>{
-    //   const imgs = ref(imgDB, `Imgs/${v4()}`)
-    //   uploadBytes(imgs,e.target.files[0]).then(data=>{
-    //     getDownloadURL(data.ref).then(val=>{
-    //       setImg(val)
-    //     })
-    //   })
-    // }
-
-    return(
-        <div>
-            <form>
-                <input type="text" placeholder="Search..."/>
-                <button>Search</button>
-            </form>
-            <form  onSubmit={handleSubmit}>
-            <div className="card">
-                                <br/>
-                                <input type="file" accept="image/JPEG, image/png, image/jpg" id="input-file" onChange={(e)=>setImg(e.target.files[0])}/>
-                                <br/>
-                                <button onClick={handleClick}>Add hotel img</button>
-                            </div>
-                            <input type="file" onChange={(e)=>handleUpload(e)}/>
-                            <label htmlFor="fname">Room Type:</label><br/>
-                            <input type="text" placeholder='Enter text' name="room" value={room.room} onChange={handleInputChange}/>
-                           <label htmlFor="fname">Add description:</label><br/>
-                            <input type="text" placeholder='Enter text'name="description" value={room.description} onChange={handleInputChange}/>
-                            <label htmlFor="fname">Add Price:</label><br/>
-                            <input type="number" placeholder='Enter text'name="price" value={room.price} onChange={handleInputChange}/>
-                            <label htmlFor="fname">Capacity:</label><br/>
-                            <input type="text" placeholder='Enter text' name="capacity" value={room.capacity} onChange={handleInputChange}/>
-                            <label htmlFor="fname">Availability:</label><br/>
-                            <input type="text" placeholder='Enter text' name="availability" value={room.availability} onChange={handleInputChange}/>
-                            <button type="submit" onClick={handleSubmit}>Add Room</button>
-            </form>
-                            <div className="grid">
-                    <div className="col">
-                      <Card>
-                      {Object.values(room).map((value, index) => (
-    <div key={index}>
-          <>
-          <Image src='/' wrapped ui={false} />
-    <CardContent>
-      <CardHeader>{value.room}</CardHeader>
-      <CardMeta>
-        <span className='date'>{value.price}</span>
-      </CardMeta>
-      <CardDescription>
-        {value.description}
-      </CardDescription>
-    </CardContent>
-    <CardContent extra>
-      <a>
-        <Icon name='user' />
-        {value.capacity}
-      </a>
-    </CardContent>
-          </>
-    </div>
-))}
-
-  </Card>
-  </div>
-                    <div className="col"></div>
-                    <div className="col"></div>
-                    <div className="col"></div>
-                    <div className="col">
-                    <a href="http://www.google.com">
-                    <img src="./images/arrow-icon-bg.png" width="200" />
-                    </a>
-                    </div>
-                </div>
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Search..." />
+        <button>Search</button>
+      </form>
+      <form onSubmit={handleSubmit}>
+        <div className="card">
+          <br />
+          <input
+            type="file"
+            accept="image/jpeg, image/png, image/jpg"
+            id="input-file"
+            onChange={(e) => setImg(e.target.files[0])}
+          />
+          <br />
+          <button type="button" onClick={handleClick}>
+            Add hotel img
+          </button>
         </div>
-    )
+        <label htmlFor="inputRoomType">Room Type:</label>
+        <br />
+        <input
+          type="text"
+          placeholder="Enter text"
+          name="room"
+          id="inputRoomType"
+          value={room.room}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="inputDescription">Add description:</label>
+        <br />
+        <input
+          type="text"
+          placeholder="Enter text"
+          name="description"
+          id="inputDescription"
+          value={room.description}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="inputPrice">Add Price:</label>
+        <br />
+        <input
+          type="number"
+          placeholder="Enter price"
+          name="price"
+          id="inputPrice"
+          value={room.price}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="inputCapacity">Capacity:</label>
+        <br />
+        <input
+          type="text"
+          placeholder="Enter capacity"
+          name="capacity"
+          id="inputCapacity"
+          value={room.capacity}
+          onChange={handleInputChange}
+        />
+        <label htmlFor="inputAvailability">Availability:</label>
+        <br />
+        <input
+          type="text"
+          placeholder="Enter availability"
+          name="availability"
+          id="inputAvailability"
+          value={room.availability}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Add Room</button>
+      </form>
+
+      <div className="grid">
+        {/* Card layout goes here */}
+      </div>
+    </div>
+  );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,114 +1,229 @@
-import React from "react";
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/NavBar";
-import {app, database, imageDb} from '../firebaseConfig';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { app, database } from "../firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 import { signOut } from "firebase/auth";
 import {
-    CardMeta,
-    CardHeader,
-    CardDescription,
-    CardContent,
-    Card,
-    Icon,
-    Image,
-  } from 'semantic-ui-react';
-  import {useNavigation, Link} from "react-router-dom";
+  Card,
+  Icon,
+  Image,
+  Button,
+  CardContent,
+  CardHeader,
+  CardMeta,
+  CardDescription,
+} from "semantic-ui-react";
+import { useNavigate } from "react-router-dom";
 
-export default function HomePage(){
-    const myStyle = {
-        backgroundImage:
-            "url('./images/swimming-pool.jpg')",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
+
+export default function HomePage() {
+  const myStyle = {
+    backgroundImage: "url('./images/swimming-pool.jpg')",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
+
+  const collectionStore = collection(database, "rooms");
+  const navigate = useNavigate();
+
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const snapshot = await getDocs(collectionStore);
+        const roomsArray = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log("Rooms Data:", roomsArray);
+        setRooms(roomsArray);
+      } catch (error) {
+        console.error("Error fetching rooms:", error);
+      }
     };
 
-    const collectionStore = collection(database, 'rooms');
-
-    const [room, setRoom] = useState({
-      img: "",
-      room:"",
-      description: "",
-      price: "",
-      capacity: "",
-      availability: "",
-  });
-
-    useEffect(() => {
-      const fetchRooms = () => {
-          getDocs(collectionStore)
-              .then((snapshot) => {
-                  const roomsArray = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-                  console.log("Rooms Data:", roomsArray);
-                  setRoom(roomsArray);
-              })
-              .catch((error) => {
-                  console.error('Error fetching rooms:', error);
-              });
-      };
-      fetchRooms();
+    fetchRooms();
   }, []);
 
-  function Signout(){
-    signOut();
-  }
+  const handleBooking = (roomId) => {
+    //alert(`Room ${roomId} booked successfully!`);
+    navigate("/PaymentPage", { state: { roomId } });
+  };
+
+  return (
+    <div>
+      <div className="top"></div>
+      <div className="main">
+        <div className="grid">
+          {rooms.map((room) => (
+            <div className="col" key={room.id}>
+              <Card>
+                <Image
+                  src={room.img || "./images/default-room.jpg"}
+                  wrapped
+                  ui={false}
+                  alt="Room"
+                />
+                <CardContent>
+                  <CardHeader>{room.room}</CardHeader>
+                  <CardMeta>
+                    <span className="date">${room.price} per night</span>
+                  </CardMeta>
+                  <CardDescription>{room.description}</CardDescription>
+                </CardContent>
+                <CardContent extra>
+                  <a>
+                    <Icon name="user" />
+                    Capacity: {room.capacity}
+                  </a>
+                  <span style={{ marginLeft: "10px" }}>
+                    {room.availability ? "Available" : "Not Available"}
+                  </span>
+                </CardContent>
+                <Button
+  color="blue"
+  onClick={() => handleBooking(room.id)} 
+  disabled={!room.availability} 
+>
+  Book Now
+</Button>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </div>
+      <footer>
+        <h6>Contacts</h6>
+        <ul>
+          <li>Email: theoceanviewpalace@gmail.com</li>
+          <li>Call: 087 043 9576</li>
+        </ul>
+        <Button color="red" onClick={signOut}>
+          Sign Out
+        </Button>
+      </footer>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+// import React from "react";
+// import { useState, useEffect } from 'react';
+// import Navbar from "../components/NavBar";
+// import {app, database, imageDb} from '../firebaseConfig';
+// import { collection, addDoc, getDocs } from 'firebase/firestore';
+// import { signOut } from "firebase/auth";
+// import {
+//     CardMeta,
+//     CardHeader,
+//     CardDescription,
+//     CardContent,
+//     Card,
+//     Icon,
+//     Image,
+//   } from 'semantic-ui-react';
+//   import {useNavigation, Link} from "react-router-dom";
+
+// export default function HomePage(){
+//     const myStyle = {
+//         backgroundImage:
+//             "url('./images/swimming-pool.jpg')",
+//         backgroundSize: "cover",
+//         backgroundRepeat: "no-repeat",
+//     };
+
+//     const collectionStore = collection(database, 'rooms');
+
+//     const [room, setRoom] = useState({
+//       img: "",
+//       room:"",
+//       description: "",
+//       price: "",
+//       capacity: "",
+//       availability: "",
+//   });
+
+//     useEffect(() => {
+//       const fetchRooms = () => {
+//           getDocs(collectionStore)
+//               .then((snapshot) => {
+//                   const roomsArray = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+//                   console.log("Rooms Data:", roomsArray);
+//                   setRoom(roomsArray);
+//               })
+//               .catch((error) => {
+//                   console.error('Error fetching rooms:', error);
+//               });
+//       };
+//       fetchRooms();
+//   }, []);
+
+//   function Signout(){
+//     signOut();
+//   }
 
   
     
 
-    return(
-        <div>
-            {/* <form className="search-form">
-            <input type="search" id="gsearch" name="gsearch"/>
-            <button>Search</button>
-            </form> */}
-            <div className="top">
-                 </div>
-            <div className="main">
-                <div className="grid">
-                    <div className="col"><Card>
-                    {Object.values(room).map((value, index) => (
-    <div key={index}>
-          <>
-          <Image src='/' wrapped ui={false} />
-    <CardContent>
-      <CardHeader>{value.room}</CardHeader>
-      <CardMeta>
-        <span className='date'>{value.price}</span>
-      </CardMeta>
-      <CardDescription>
-        {value.description}
-      </CardDescription>
-    </CardContent>
-    <CardContent extra>
-      <a>
-        <Icon name='user' />
-        {value.capacity}
-      </a>
-    </CardContent>
-          </>
-    </div>
-))}
-  </Card></div>
-                    {/* <div className="col"><img src="hotel-room.jpg"width="150"/></div>
-                    <div className="col"><img src="hotel-room.jpg" width="150"/></div>
-                    <div className="col"><img src="hotel-room.jpg" width="150"/></div> */}
-                    <div className="col">
-                    <a href="http://www.google.com">
-                    <img src="./images/arrow-icon-bg.png" />
-                    </a>
-                    </div>
-                    {/* <button><Link to="PaymentPage">Don't have an account : Sign Up</Link></button> */}
-                </div>
-            </div>
-            <footer>
-                <h6>Contacts</h6>
-                <ul>
-                    <li>Email: theoceanviewpalace@gmail.com</li>
-                    <li>Call: 087 043 9576</li>
-                </ul>
-                <button>Sign Out</button>
-            </footer>
-        </div>
-    )
-}
+//     return(
+//         <div>
+//             {/* <form className="search-form">
+//             <input type="search" id="gsearch" name="gsearch"/>
+//             <button>Search</button>
+//             </form> */}
+//             <div className="top">
+//                  </div>
+//             <div className="main">
+//                 <div className="grid">
+//                     <div className="col"><Card>
+//                     {Object.values(room).map((value, index) => (
+//     <div key={index}>
+//           <>
+//           <Image src='/' wrapped ui={false} />
+//     <CardContent>
+//       <CardHeader>{value.room}</CardHeader>
+//       <CardMeta>
+//         <span className='date'>{value.price}</span>
+//       </CardMeta>
+//       <CardDescription>
+//         {value.description}
+//       </CardDescription>
+//     </CardContent>
+//     <CardContent extra>
+//       <a>
+//         <Icon name='user' />
+//         {value.capacity}
+//       </a>
+//     </CardContent>
+//           </>
+//     </div>
+// ))}
+//   </Card></div>
+//                     {/* <div className="col"><img src="hotel-room.jpg"width="150"/></div>
+//                     <div className="col"><img src="hotel-room.jpg" width="150"/></div>
+//                     <div className="col"><img src="hotel-room.jpg" width="150"/></div> */}
+//                     <div className="col">
+//                     <a href="http://www.google.com">
+//                     <img src="./images/arrow-icon-bg.png" />
+//                     </a>
+//                     </div>
+//                     {/* <button><Link to="PaymentPage">Don't have an account : Sign Up</Link></button> */}
+//                 </div>
+//             </div>
+//             <footer>
+//                 <h6>Contacts</h6>
+//                 <ul>
+//                     <li>Email: theoceanviewpalace@gmail.com</li>
+//                     <li>Call: 087 043 9576</li>
+//                 </ul>
+//                 <button>Sign Out</button>
+//             </footer>
+//         </div>
+//     )
+// }
